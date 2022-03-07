6fd34ba714385f0d07b46ca8e550294c3370da49
ve7gel <o31254G!>
2022-03-07 13:52:54 -0800

    First draft

--- presence-poly.py
+++ presence-poly.py
@@ -1,95 +1,125 @@
 #!/usr/bin/env python3
-import polyinterface
+import udi_interface
 import sys
 import time
-import bluetooth
-import bluetooth._bluetooth as bt
+import bluetooth as bt
 import struct
 import array
 import fcntl
 import os
 
-LOGGER = polyinterface.LOGGER
+LOGGER = udi_interface.LOGGER
+Custom = udi_interface.Custom
 
-class PresenceController(polyinterface.Controller):
 
-    def __init__(self, polyglot):
-        super(PresenceController, self).__init__(polyglot)
+class Controller(udi_interface.Node):
+    id = 'presence_controller'
+
+    def __init__(self, polyglot, parent, address, name):
+        super(Controller, self).__init__(polyglot, parent, address, name)
         self.name = 'Presence Controller'
         self.firstRun = True
-        
+        self.poly = polyglot
+        self.name = name
+        self.address = address
+        self.primary = parent
+        self.configured = False
+
+        self.Parameters = Custom(polyglot, 'customparams')
+        self.Notices = Custom(polyglot, 'notices')
+
+        # self.poly.subscribe(self.poly.CONFIG, self.configHandler)
+        self.poly.subscribe(self.poly.CUSTOMPARAMS, self.check_params)
+        self.poly.subscribe(self.poly.START, self.start, address)
+        self.poly.subscribe(self.poly.POLL, self.poll)
+        self.poly.subscribe(self.poly.LOGLEVEL, self.handleLevelChange)
+
+        self.poly.subscribe(self.poly.STOP, self.stop_handler)
+        # self.poly.subscribe(self.poly.ADDNODEDONE, self.node_queue)
+
+        self.poly.ready()
+        self.poly.addNode(self)
+
     def start(self):
         LOGGER.info('Presence Controller started.')
-        self.check_params()
         self.discover()
         self.setDriver('ST', 1)
-        
+
+    def poll(self, polltype):
+
+        if not self.configured:
+            return
+        if 'shortPoll' in polltype:
+            self.shortPoll()
+
     def shortPoll(self):
-        #This is where the updates to each node happen
-        for node in self.nodes:
-            self.nodes[node].update()
+        # This is where the updates to each node happen
+        for node in self.poly.nodes:
+            self.poly.nodes[node].update()
         if self.firstRun:
             self.query()
             self.firstRun = False
-            
+
     def longPoll(self):
-        #Not used
+        # Not used
         pass
 
     def query(self):
-        for node in self.nodes:
-            self.nodes[node].reportDrivers()
+        for node in self.poly.nodes:
+            self.poly.nodes[node].reportDrivers()
 
-    def discover(self, *args, **kwargs):
-        #Discover nodes and add them by type
-        for key,val in self.polyConfig['customParams'].items():
-            #LOGGER.debug(key + " => " + val)
-            if (val.find(':') != -1):
-                blueid = val.replace(':','').lower()
-                self.addNode(BluetoothNode(self, self.address, blueid, key))
-            elif (val.find('.') != -1):
-                netip = val.replace('.','')
-                self.addNode(NetworkNode(self, self.address, netip, val, key))
+    def discover(self, parameters):
+        self.Parameters.load(parameters)
+        # Discover nodes and add them by type
+        for key, val in self.polyConfig['customParams'].items():
+            LOGGER.debug(key + " => " + val)
+            if val.find(':') != -1:
+                blueid = val.replace(':', '').lower()
+                # self.poly.addNode(BluetoothNode(self, self.address, blueid, key))
+            elif val.find('.') != -1:
+                netip = val.replace('.', '')
+                self.poly.addNode(NetworkNode(self, self.address, netip, val, key))
 
     def update(self):
-        pass    
+        pass
+
+    def handleLevelChange(self, level):
+        LOGGER.info('New log level: {}'.format(level))
 
     def delete(self):
         LOGGER.info('Deleted')
 
-    def stop(self):
+    def stop_handler(self):
         self.setDriver('ST', 0)
         LOGGER.debug('Presence Controller stopped.')
 
-    def check_params(self):
+    def check_params(self, config):
+        self.Parameters.load(config)
+        self.discover()
         # Remove all existing notices
-        self.removeNoticesAll()
-        
-    def remove_notices_all(self,command):
-        LOGGER.info('remove_notices_all:')
-        # Remove all existing notices
-        self.removeNoticesAll()
+        self.Notices.clear()
 
-    def update_profile(self,command):
+    def update_profile(self, command):
         LOGGER.info('update_profile:')
         st = self.poly.installprofile()
         return st
 
-    id = 'presence_controller'
     commands = {
         'DISCOVER': discover,
         'UPDATE_PROFILE': update_profile,
-        'REMOVE_NOTICES_ALL': remove_notices_all
     }
     drivers = [
         {'driver': 'ST', 'value': 0, 'uom': 2}
     ]
 
+
+'''
 class BlueHelper(object):
 
     def __init__(self, addr):
-        #Initializes bluetooth object
+        # Initializes bluetooth object
         self.addr = addr
+
         self.hci_sock = bt.hci_open_dev()
         self.hci_fd = self.hci_sock.fileno()
         self.bt_sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
@@ -98,21 +128,21 @@ class BlueHelper(object):
         self.cmd_pkt = None
 
     def prepare_command(self):
-        #Creates the command
+        # Creates the command
         reqstr = struct.pack(
-            "6sB17s", bt.str2ba(self.addr), bt.ACL_LINK, bytes("\0",'utf-8') * 17)
+            "6sB17s", bt.str2ba(self.addr), bt.ACL_LINK, bytes("\0", 'utf-8') * 17)
         request = array.array("b", reqstr)
         handle = fcntl.ioctl(self.hci_fd, bt.HCIGETCONNINFO, request, 1)
         handle = struct.unpack("8xH14x", request.tobytes())[0]
         self.cmd_pkt = struct.pack('H', handle)
 
     def connect(self):
-        #Connects to the bluetooth device
+        # Connects to the bluetooth device
         self.bt_sock.connect_ex((self.addr, 1))
         self.connected = True
 
     def get_rssi(self):
-        #Gets the RSSI value
+        # Gets the RSSI value
         try:
             # Only do connection if not already connected
             if not self.connected:
@@ -127,53 +157,54 @@ class BlueHelper(object):
             return rssi
         except IOError as ioerr:
             # Happens if connection fails
-            #LOGGER.debug("I/O error: {0}".format(ioerr))
+            # LOGGER.debug("I/O error: {0}".format(ioerr))
             self.connected = False
             return None
 
-class BluetoothNode(polyinterface.Node):
+
+class BluetoothNode(udi_interface.Node):
     def __init__(self, controller, primary, address, name):
         super(BluetoothNode, self).__init__(controller, primary, address, name)
-        self.blueid = ':'.join(self.address[i:i+2] for i in range(0, len(self.address), 2)).upper()
+        self.blueid = ':'.join(self.address[i:i + 2] for i in range(0, len(self.address), 2)).upper()
         self.scan = 1
         self.proximity = 0
 
     def start(self):
         self.setOn('DON')
-        
+
     def update(self):
-        if (self.scan):
+        if self.scan:
             btnode = BlueHelper(addr=self.blueid)
             result = btnode.get_rssi()
-            if (result != None):
+            if result is not None:
                 LOGGER.debug('Bluetooth ' + self.blueid + ': In range. RSSI: ' + str(result))
-                if (result >=0):
+                if result >= 0:
                     self.setInRange(5)
-                elif (result < 0 and result >= -5):
+                elif 0 > result >= -5:
                     self.setInRange(4)
-                elif (result < -5 and result >= -15):
+                elif -5 > result >= -15:
                     self.setInRange(3)
-                elif (result < -15 and result >= -35):
+                elif -15 > result >= -35:
                     self.setInRange(2)
-                elif (result < -35):
+                elif result < -35:
                     self.setInRange(1)
-            elif (self.proximity > 1):
+            elif self.proximity > 1:
                 self.setInRange(self.proximity - 1)
                 LOGGER.debug('Bluetooth ' + self.blueid + ': In Fault')
-            elif (self.proximity == 1):
+            elif self.proximity == 1:
                 LOGGER.debug('Bluetooth ' + self.blueid + ': Out of range')
                 self.setOutRange()
-            
-    def setInRange(self,prox):
+
+    def setInRange(self, prox):
         self.setDriver('ST', 1)
         self.proximity = prox
         self.setDriver('GV0', self.proximity)
-        
+
     def setOutRange(self):
         self.setDriver('ST', 0)
         self.proximity = 0
         self.setDriver('GV0', self.proximity)
-    
+
     def setOn(self, command):
         self.setOutRange()
         self.setDriver('GV1', 1)
@@ -186,7 +217,6 @@ class BluetoothNode(polyinterface.Node):
 
     def query(self):
         self.reportDrivers()
-
 
     drivers = [
         {'driver': 'ST', 'value': 0, 'uom': 2},
@@ -197,8 +227,10 @@ class BluetoothNode(polyinterface.Node):
     id = 'bluetooth_node'
 
     commands = {
-                    'DON': setOn, 'DOF': setOff
-                }
+        'DON': setOn, 'DOF': setOff
+    }
+'''
+
 
 class PingHelper(object):
 
@@ -208,7 +240,7 @@ class PingHelper(object):
 
     def ping(self):
         try:
-            response = os.system("ping -c 1 -W " + str(self.timeout-1) + " " + self.ip)
+            response = os.system("ping -c 1 -W " + str(self.timeout - 1) + " " + self.ip)
             if response == 0:
                 return response
             else:
@@ -216,42 +248,44 @@ class PingHelper(object):
         except:
             # Capture any exception
             return None
-                
-class NetworkNode(polyinterface.Node):
+
+
+class NetworkNode(udi_interface.Node):
 
     def __init__(self, controller, primary, address, ipaddress, name):
         super(NetworkNode, self).__init__(controller, primary, address, name)
         self.ip = ipaddress
         self.scan = 1
         self.strength = 0
-        
+
     def start(self):
         self.setOn('DON')
-        
+
     def update(self):
-        if (self.scan):
-            onnet = PingHelper(ip=self.ip,timeout=self.parent.polyConfig['shortPoll'])
+        if self.scan:
+
+            onnet = PingHelper(ip=self.ip, timeout=self.primary.polyConfig['shortPoll'])
             result = onnet.ping()
-            if (result != None):
+            if result is not None:
                 LOGGER.debug('Network ' + self.ip + ': On Network')
                 self.setOnNetwork(5)
-            elif (self.strength > 1):
+            elif self.strength > 1:
                 self.setOnNetwork(self.strength - 1)
                 LOGGER.debug('Network ' + self.ip + ': In Fault')
-            elif (self.strength == 1):
+            elif self.strength == 1:
                 LOGGER.debug('Network ' + self.ip + ': Out of Network')
                 self.setOffNetwork()
-            
-    def setOnNetwork(self,strength):
+
+    def setOnNetwork(self, strength):
         self.setDriver('ST', 1)
         self.strength = strength
         self.setDriver('GV0', self.strength)
-        
+
     def setOffNetwork(self):
         self.setDriver('ST', 0)
         self.strength = 0
         self.setDriver('GV0', self.strength)
-    
+
     def setOn(self, command):
         self.setOffNetwork()
         self.setDriver('GV1', 1)
@@ -265,7 +299,6 @@ class NetworkNode(polyinterface.Node):
     def query(self):
         self.reportDrivers()
 
-
     drivers = [
         {'driver': 'ST', 'value': 0, 'uom': 2},
         {'driver': 'GV0', 'value': 0, 'uom': 56},
@@ -275,14 +308,15 @@ class NetworkNode(polyinterface.Node):
     id = 'network_node'
 
     commands = {
-                    'DON': setOn, 'DOF': setOff
-                }
+        'DON': setOn, 'DOF': setOff
+    }
+
 
 if __name__ == "__main__":
     try:
-        polyglot = polyinterface.Interface('PresenceController')
+        polyglot = udi_interface.Interface([])
         polyglot.start()
-        control = PresenceController(polyglot)
-        control.runForever()
+        control = Controller(polyglot, 'controller', 'controller', 'presence')
+        polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
         sys.exit(0)
